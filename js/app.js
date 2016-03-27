0148572f8c60f7ec2654e091dea93a13c561ef58
Carlo Lobrano <c.lobrano@gmail.com>
2016-03-28 01:40:25 +0200

    First commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    console.log("Getting first quote");
+    getQuote();
+    $("button").click(getQuote);
+});
+
+function getQuote() {
+    console.log("Refresh quote");
+    $.getJSON("http://quotes.rest/qod.json", function(data) {
+        $("#quote").html(data.contents.quotes[0].quote);
+        $("#author").html("- " + data.contents.quotes[0].author);
+    });
+}
