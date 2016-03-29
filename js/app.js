8622bed7569a4baa80db5d8d44c41e6b805b1974
Carlo Lobrano <c.lobrano@gmail.com>
2016-03-29 19:25:49 +0200

    Replace Quotes source

--- js/app.js
+++ js/app.js
@@ -1,12 +1,23 @@
-$(document).ready(function(){
-    getQuote();
-    $("button").click(getQuote);
+$(document).ready(function() {
+  getQuote();
+  $("button").click(getQuote);
 });
 
 function getQuote() {
-    console.log("Refresh quote");
-    $.getJSON("//quotes.rest/qod.json", function(data) {
-        $("#quote").html(data.contents.quotes[0].quote);
-        $("#author").html("- " + data.contents.quotes[0].author);
-    });
+  $.ajax({
+    type: "POST",
+    beforeSend: function(request) {
+      request.setRequestHeader("X-Mashape-Key", 'NnNuEd4mlkmshGIlZfWqobpVSNoxp1ejI05jsn5Hrtt3XgvSaS');
+    },
+    url: "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous",
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: function(response) {
+      $("#quote").html(response.quote);
+      $("#author").html("- " + response.author);
+    },
+    error: function(obj, message, exception) {
+      console.log(message + " " + exception);
+    }
+  });
 }
