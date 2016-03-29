bfc22f54781574b303cf603c331679fb37a07bea
Carlo Lobrano <c.lobrano@gmail.com>
2016-03-29 08:19:41 +0200

    Removed http tag from json request

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 
 function getQuote() {
     console.log("Refresh quote");
-    $.getJSON("http://quotes.rest/qod.json", function(data) {
+    $.getJSON("//quotes.rest/qod.json", function(data) {
         $("#quote").html(data.contents.quotes[0].quote);
         $("#author").html("- " + data.contents.quotes[0].author);
     });
