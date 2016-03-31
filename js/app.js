94d191b19772ff79441cea1d7459c4840b4ebed6
Carlo Lobrano <c.lobrano@gmail.com>
2016-03-31 22:39:53 +0200

    Replaced random quote site

--- js/app.js
+++ js/app.js
@@ -1,30 +1,13 @@
 $(document).ready(function() {
-  getQuote();
-  $("button").click(getQuote);
-  tweet()
+    $("button").click(function() {
+        location.reload();
+    })
 });
 
-function getQuote() {
-  $.ajax({
-    type: "POST",
-    beforeSend: function(request) {
-      request.setRequestHeader("X-Mashape-Key", 'NnNuEd4mlkmshGIlZfWqobpVSNoxp1ejI05jsn5Hrtt3XgvSaS');
-    },
-    url: "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous",
-    contentType: "application/json; charset=utf-8",
-    dataType: "json",
-    success: function(response) {
-      $("#quote").html(response.quote);
-      $("#author").html("- " + response.author);
-    },
-    error: function(obj, message, exception) {
-      console.log(message + " " + exception);
-    }
-  });
+function parseJsonpResponse(response) {
+    console.log("callback");
+    $("#quote").html(response[0].content);
+    $("#author").html(response[0].title);
 }
 
 
-function tweet() {
-    var quote = $("#quote").clone();
-    console.log("Quote is: " + quote);
-}
